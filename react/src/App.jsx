bc6bfe6715831745d9732416a5c0b30b1b272b40
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-12-18 22:53:24 +0200

    landing page, navbar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 			
 			<Navbar />
 
-			<div className="px-16 mt-8">
+			<div className=" flex px-16 mt-8">
 				<div className="flex flex-col gap-4"> 
 					<h1 className=" font-montserrat font-extrabold text-[48px] w-1/2">PAY ALL YOUR <span className=" text-primaryCol">TICKETS</span> AND <span className=" text-primaryCol">FINES</span> IN ONE PLACE ON A GO 🚀 </h1>
 					<p className=" w-1/3 text-lg">Settle fines swiftly, click to pay - Your hassle-free solution for resolving police tickets online!</p>
