f0a6ff61558000f8c63a86c4b1f37937e5ee1610
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-12-19 10:19:30 +0200

    ....

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -4,7 +4,11 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
+   <!-- Tailwind -->
    <script src="https://cdn.tailwindcss.com"></script>
+
+   <!-- Font awesome all icons  -->
+   <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.3.0/css/all.css">
 
    <title>TicketPay</title>
 </head>
